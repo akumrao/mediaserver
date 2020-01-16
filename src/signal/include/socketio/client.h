5dae54cb918b508413bb15c47bc79e1782bc5bcd
Arvind Umrao <arvind.umrao@sococo.com>
2020-01-16 19:46:45 +0000

    added header of socketio

---
+++ src/signal/include/socketio/client.h
@@ -0,0 +1,251 @@
+/*
+  The following events are reserved and should not be used as event names by your application:
+ error
+connect
+disconnect
+disconnecting
+newListener
+removeListener
+ping
+pong
+ 
+ 
+ */
+
+#ifndef SocketIO_Client_H
+#define SocketIO_Client_H
+
+
+#include "socketio/packet.h"
+
+#include "net/netInterface.h"
+#include "http/websocket.h"
+#include "json/json.h"
+
+#include "base/logger.h"
+
+#include "base/Timer.h"
+#include "base/collection.h"
+#include <exception>
+
+
+
+#include "http/client.h"
+#include "base/logger.h"
+#include "base/application.h"
+#include "base/platform.h"
+#include "http/HttpClient.h"
+#include "http/HttpsClient.h"
+
+namespace base {
+    namespace sockio {
+
+           /****************************************************************************/
+
+        class event {
+        public:
+            const std::string& get_nsp() const;
+            const std::string& get_name() const;
+            const json& get_message() const;
+
+            bool need_ack() const;
+
+            void put_ack_message(json const& ack_message);
+
+            json const& get_ack_message() const;
+            json& get_ack_message_impl();
+
+        protected:
+            event(std::string const& nsp, std::string const& name, json const& messages, bool need_ack);
+            event(std::string const& nsp,std::string const& name, json&& messages,bool need_ack);
+
+       
+        private:
+            const std::string m_nsp;
+            const std::string m_name;
+            const json m_messages;
+            const bool m_need_ack;
+            json m_ack_message;
+
+            friend class event_adapter;
+        };
+
+
+        class socket;
+
+        class Client  {
+        public:
+
+            enum con_state
+            {
+                con_opening,
+                con_opened,
+                con_closing,
+                con_closed
+            };
+            Client(const std::string& host, uint16_t port);
+            virtual ~Client();
+
+            virtual void connect(const std::string& host, uint16_t port);
+            virtual void connect();
+            // virtual void close();
+
+
+            std::map<const std::string, socket*> m_sockets;
+           
+            void remove_socket(string const& nsp);
+         
+            std::mutex m_socket_mutex;
+
+            void close_impl(int const& code, string const& reason);
+            
+             void ping();
+             void timeout_pong();
+             void timeout_reconnect();
+             con_state m_con_state;
+              void on_close();
+         
+            //virtual const char* className() const { return "SocketIOClient"; }
+            std::string const& get_sessionid();
+            // void on_message_packet(packet const& p);
+            std::function<void(socket* sock) > cbConnected;
+
+            void send(packet& p);
+
+            socket* io(string const& nsp);
+
+        protected:
+            // virtual void setError(const Errors& error);
+
+            virtual void reset();
+
+            virtual void sendHandshakeRequest();
+
+            void on_decode(packet const& pack);
+            void on_encode(bool isBinary, shared_ptr<const string> const& payload);
+
+            void on_handshake(json const& message);
+
+            void on_pong();
+
+            void clear_timers();
+
+
+        protected:
+            socket* get_socket(string const& nsp);
+            std::string _host;
+            uint16_t _port;
+    
+            Timer m_ping_timer{ nullptr};
+            Timer m_ping_timeout_timer{ nullptr};
+            Timer m_reconn_timer{ nullptr};
+
+
+            std::string m_sid;
+            net::ClientConnecton *m_client;
+
+            unsigned int m_ping_interval;
+            unsigned int m_ping_timeout;
+            packet_manager m_packet_mgr;
+        };
+
+
+      
+
+
+        /********************************************************/
+        class packet;
+
+        //The name 'socket' is taken from concept of official socket.io.
+
+        class socket {
+        public:
+            typedef std::function<void(const std::string& name, json const& message, bool need_ack, json& ack_message) > event_listener_aux;
+
+            typedef std::function<void(event& event) > event_listener;
+
+            typedef std::function<void(json const& message) > error_listener;
+
+            //typedef std::shared_ptr<socket> ptr;
+
+            ~socket();
+
+            void on(std::string const& event_name, event_listener const& func);
+
+            void on(std::string const& event_name, event_listener_aux const& func);
+
+            void off(std::string const& event_name);
+
+            void off_all();
+
+            void close();
+
+            void on_error(error_listener const& l);
+
+            void off_error();
+
+            std::map<unsigned int, std::function<void (json const&) > > m_acks;
+            void emit(std::string const& name, std::string const& msglist = "", std::function<void (json const&)> const& ack = nullptr);
+            void on_socketio_event(const std::string& nsp, int msgId, const std::string& name, json && message);
+            
+            void ack(int msgId,string const& name,json const& ack_message);
+            void on_socketio_ack(int msgId, json const& message);
+
+            event_listener get_bind_listener_locked(const string &event);
+
+            std::string const& get_namespace() const;
+
+            socket(Client*, std::string const&);
+
+            std::map<std::string, event_listener> m_event_binding;
+        protected:
+
+            void on_connected();
+           
+            void timeout_connection();
+
+            error_listener m_error_listener;
+
+            void on_close();
+
+            void on_open();
+
+            void on_disconnect();
+
+            void on_message_packet(packet const& p);
+            void on_socketio_error(json const& err_message);
+            void send_connect();
+            void send_packet(packet &p);
+
+
+            friend class Client;
+
+        private:
+            //disable copy constructor and assign operator.
+
+            socket(socket const&) {
+            }
+
+            void operator=(socket const&) {
+            }
+
+            const std::string m_nsp;
+
+            Client *m_client;
+
+            Timer m_connection_timer{ nullptr};
+            bool m_connected{ false};
+            std::queue<packet> m_packet_queue;
+
+            std::mutex m_packet_mutex;
+            std::mutex m_event_mutex;
+        };
+
+    }
+} // namespace scy::sockio
+
+
+#endif //  SocketIO_Client_H
+
+
+
---
+++ src/signal/include/socketio/packet.h
@@ -0,0 +1,121 @@
+
+/*
+  The following events are reserved and should not be used as event names by your application:
+ error
+connect
+disconnect
+disconnecting
+newListener
+removeListener
+ping
+pong
+ 
+ 
+ */
+
+
+#ifndef SocketIO_Packet_H
+#define SocketIO_Packet_H
+
+#include "json/json.h"
+
+using namespace std;
+
+namespace base {
+namespace sockio {
+
+class packet
+    {
+    public:
+        enum frame_type
+        {
+            frame_open = 0,
+            frame_close = 1,
+            frame_ping = 2,
+            frame_pong = 3,
+            frame_message = 4,
+            frame_upgrade = 5,
+            frame_noop = 6
+        };
+        
+        enum type
+        {
+            type_min = 0,
+            type_connect = 0,
+            type_disconnect = 1,
+            type_event = 2,
+            type_ack = 3,
+            type_error = 4,
+            type_binary_event = 5,
+            type_binary_ack = 6,
+            type_max = 6,
+            type_undetermined = 0x10 //undetermined mask bit
+        };
+    private:
+        frame_type _frame;
+        int _type;
+        string _nsp;
+        int _pack_id;
+        json _message;
+        unsigned _pending_buffers;
+        vector<shared_ptr<const string> > _buffers;
+    public:
+        packet(string const& nsp,json const& msg,int pack_id = -1,bool isAck = false);//message type constructor.
+        
+        packet(frame_type frame);
+        
+        packet(type type,string const& nsp= string(),json const& msg = json());//other message types constructor.
+        //empty constructor for parse.
+        packet();
+        
+        frame_type get_frame() const;
+        
+        type get_type() const;
+        
+        bool parse(string const& payload_ptr);//return true if need to parse buffer.
+        
+        bool parse_buffer(string const& buf_payload);
+        
+        bool accept(string& payload_ptr, vector<shared_ptr<const string> >&buffers); //return true if has binary buffers.
+        
+        string const& get_nsp() const;
+        
+        json const& get_message() const;
+        
+        unsigned get_pack_id() const;
+        
+        static bool is_message(string const& payload_ptr);
+        static bool is_text_message(string const& payload_ptr);
+        static bool is_binary_message(string const& payload_ptr);
+    };
+    
+ class packet_manager
+    {
+    public:
+        typedef function<void (bool,shared_ptr<const string> const&)> encode_callback_function;
+        typedef  function<void (packet const&)> decode_callback_function;
+        
+        void set_decode_callback(decode_callback_function const& decode_callback);
+
+        void set_encode_callback(encode_callback_function const& encode_callback);
+        
+        void encode(packet& pack,encode_callback_function const& override_encode_callback = encode_callback_function()) const;
+        
+        void put_payload(string const& payload);
+        
+        void reset();
+        
+    private:
+        decode_callback_function m_decode_callback;
+        
+        encode_callback_function m_encode_callback;
+        
+        std::unique_ptr<packet> m_partial_packet;
+    };
+
+
+} } // namespace base::sockio
+
+
+#endif //  SocketIO_Packet_H
+
