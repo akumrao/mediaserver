d17ba68b5ce52ef1bb41c0c465fe482b3b527b64
Arvind Umrao <arvind.umrao@sococo.com>
2020-03-05 15:11:30 +0530

    script to upload at udp port

---
+++ src/net/awsupload/uploadToAws.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import time
+import socket
+import sys
+
+serverAddressPort   = ("18.221.232.217", 51038)
+
+CHUNK_SIZE = 32 * 1024
+
+path = sys.argv[1]
+
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPClientSocket.connect(serverAddressPort)
+
+start =  time.time()
+
+with open(path, 'rb') as f:
+	data = f.read(CHUNK_SIZE)
+	while data:
+	    UDPClientSocket.sendto(data, serverAddressPort)
+	    print('Sent '+ str(CHUNK_SIZE))
+	    data = f.read(CHUNK_SIZE)
+	UDPClientSocket.close()
+
+end =  time.time()
+print( end - start)
