9a7b07efbccc3952ec0809669d61d1bd9c3fb2fb
Arvind Umrao <arvind.umrao@sococo.com>
2020-04-19 09:40:05 +0530

    added copyright messages

--- src/http/include/http/HttpsConn.h
+++ src/http/include/http/HttpsConn.h
@@ -1,8 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* This file is part of mediaserver. A webrtc sfu server.
+ * Copyright (C) 2018 Arvind Umrao <akumrao@yahoo.com> & Herman Umrao<hermanumrao@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
  */
+
 
 /* 
  * File:   HTTPConnection.h
