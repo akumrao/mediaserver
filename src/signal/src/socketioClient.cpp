e50b171ec10254c3cb01c81679f175dca6878b70
Arvind Umrao <arvind.umrao@sococo.com>
2020-04-19 09:42:00 +0530

    added copyright messages

--- src/signal/src/socketioClient.cpp
+++ src/signal/src/socketioClient.cpp
@@ -1,15 +1,13 @@
-/*
-  The following events are reserved and should not be used as event names by your application:
- error
-connect
-disconnect
-disconnecting
-newListener
-removeListener
-ping
-pong
- 
+/* This file is part of mediaserver. A webrtc sfu server.
+ * Copyright (C) 2018 Arvind Umrao <akumrao@yahoo.com> & Herman Umrao<hermanumrao@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
  */
+
 
 #include "socketio/socketioClient.h"
 #include "http/HttpsClient.h"
