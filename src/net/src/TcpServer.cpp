eb226a8a2ba9e5a933bcb608be80f00769ad1622
Arvind Umrao <akumrao@yahoo.com>
2021-10-31 14:31:29 +0530

    back port from webrtc

--- src/net/src/TcpServer.cpp
+++ src/net/src/TcpServer.cpp
@@ -182,7 +182,7 @@ namespace base
   
             SDebug << "Pipe Connected ";
 
-             ASSERT(status == 0);
+            // ASSERT(status == 0);
 
         }
     #endif   
@@ -216,7 +216,7 @@ namespace base
                 worker->obj = this;
                 
 #ifdef _WIN32
-                err = uv_thread_create(&worker->thread, workermain,      (void*)worker);
+                int err = uv_thread_create(&worker->thread, workermain,      (void*)worker);
                 if (err != 0)
                    LError("setup_workers() failed: %s", uv_strerror(err));
 
