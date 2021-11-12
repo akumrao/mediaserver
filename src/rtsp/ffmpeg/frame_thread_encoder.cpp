c39863fb67f55494a1437d54b168b103f6223e0e
sanjaykrmaurya <sanjay.kr.maurya@gmail.com>
2021-11-12 10:39:38 +0530

    Correcting compile error

--- src/rtsp/ffmpeg/frame_thread_encoder.cpp
+++ src/rtsp/ffmpeg/frame_thread_encoder.cpp
@@ -17,6 +17,8 @@
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
+#include "config.h"
+
 #ifdef HAVE_PTHREADS
 #include <pthread.h>
 #else
