630aaa1d3779f740d8a27a27ba4cfe2b353393f6
Arvind Umrao <akumrao@yahoo.com>
2021-12-12 18:30:57 +0530

    json with new camera

--- src/broadcast/main/config.js
+++ src/broadcast/main/config.js
@@ -7,11 +7,8 @@
     "potentialID" : "729795",
     "stream_type" : "webrtc",
     "rtsp": {
-        "729795F1234CAM14569": {"rtsp":"rtsp://admin:247Supp0rt!@10.86.7.224:557/RTSP2HLS/media.smp"},
-        "729795F1234CAM14236": {"rtsp":"rtsp://root:60056006@10.86.8.16:560/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
-        "729976VGSTESTHDStressBox3CAM35081": {"rtsp":"rtsp://root:60056005@10.76.0.16:555/axis-media/media.amp?videocodec=h264&resolution=1280x720&fps=25"},
-        "729976VGSTESTHDStressBox3CAM96865": {"rtsp":"rtsp://root:60056005@10.76.0.16:557/axis-media/media.amp?videocodec=h264&resolution=1280x720&fps=25"},
-        "729976VGSTESTHDStressBox3CAM86942": {"rtsp":"rtsp://root:60056005@10.76.0.16:556/axis-media/media.amp?videocodec=h264&resolution=1280x720&fps=25"}
+        "729795F1234CAM14569": {"rtsp":"rtsp://root:60056006@10.86.8.16:559/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
+        "729795F1234CAM14236": {"rtsp":"rtsp://root:60056006@10.86.8.16:560/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"}
     },
     "logLevel": "trace",
     "logTags": [
@@ -26,4 +23,3 @@
 
   }
 }
-
