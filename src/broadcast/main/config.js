9300b2bbf08cad606bf9a235ea0c173f3f86e083
Arvind Umrao <akumrao@yahoo.com>
2021-12-12 15:37:42 +0530

    changed the json format

--- src/broadcast/main/config.js
+++ src/broadcast/main/config.js
@@ -7,13 +7,13 @@
     "potentialID" : "729795",
     "stream_type" : "webrtc",
     "rtsp": {
-        "729795F1234CAM14872": "rtsp://root:60056006@10.86.8.16:606/axis-media/media.amp",
-        "729795F1234CAM14569": "rtsp://admin:247Supp0rt!@10.86.7.224:557/RTSP2HLS/media.smp",
-        "729795F1234CAM14236": "rtsp://root:60056006@10.86.8.16:560/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25",
-        "729795F1234CAM14142": "rtsp://localhost:8554/testStream",
-        "729795F1234CAM14975": "rtsp://localhost:9554/testStream"
+        "729795F1234CAM14569": {"rtsp":"rtsp://admin:247Supp0rt!@10.86.7.224:557/RTSP2HLS/media.smp"},
+        "729795F1234CAM14236": {"rtsp":"rtsp://root:60056006@10.86.8.16:560/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
+        "729976VGSTESTHDStressBox3CAM35081": {"rtsp":"rtsp://root:60056005@10.76.0.16:555/axis-media/media.amp?videocodec=h264&resolution=1280x720&fps=25"},
+        "729976VGSTESTHDStressBox3CAM96865": {"rtsp":"rtsp://root:60056005@10.76.0.16:557/axis-media/media.amp?videocodec=h264&resolution=1280x720&fps=25"},
+        "729976VGSTESTHDStressBox3CAM86942": {"rtsp":"rtsp://root:60056005@10.76.0.16:556/axis-media/media.amp?videocodec=h264&resolution=1280x720&fps=25"}
     },
-    "logLevel": "info",
+    "logLevel": "trace",
     "logTags": [
       "info"
     ],
@@ -27,6 +27,3 @@
   }
 }
 
-
-
-    
