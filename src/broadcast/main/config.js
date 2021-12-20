3a4995cae85d79ac8c0b49e884906ffca83ee4ce
Arvind Umrao <akumrao@yahoo.com>
2021-12-20 15:03:57 +0530

    config file changes

--- src/broadcast/main/config.js
+++ src/broadcast/main/config.js
@@ -7,10 +7,10 @@
     "potentialID" : "729795",
     "stream_type" : "webrtc",
     "rtsp": {
-        "729795F1234CAM14569": {"rtsp":"rtsp://root:60056006@10.86.8.16:559/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
-        "729795F1234CAM14236": {"rtsp":"rtsp://root:60056006@10.86.8.16:560/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"}
+        "729795F1234CAM14529": {"rtsp":"rtsp://root:60056005@10.76.0.16:555/axis-media/media.amp?videocodec=h264&resolution=1280x720&fps=25"},
+        "729795F1234CAM14569": {"rtsp":"rtsp://root:60056006@10.86.8.16:559/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"}
     },
-    "logLevel": "trace",
+    "logLevel": "info",
     "logTags": [
       "info"
     ],
