082e2c1af771a0e4a653fe215ee19e1d2ad8dd33
Arvind Umrao <akumrao@yahoo.com>
2021-12-21 17:23:53 +0530

    config file

--- src/broadcast/main/config.js
+++ src/broadcast/main/config.js
@@ -7,8 +7,8 @@
     "potentialID" : "729795",
     "stream_type" : "webrtc",
     "rtsp": {
-        "729795F1234CAM14529": {"rtsp":"rtsp://root:60056005@10.76.0.16:555/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
-        "729795F1234CAM14569": {"rtsp":"rtsp://root:60056006@10.86.8.16:559/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
+        "729795F1234CAM14528": {"rtsp":"rtsp://root:60056005@10.76.0.16:555/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
+        "229795F1234CAM14567": {"rtsp":"rtsp://root:60056006@10.86.8.16:559/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
         "929795F1234CAM14569": {"rtsp":"rtsp://root:60056006@10.86.8.16:560/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
         "629795F1234CAM14569": {"rtsp":"rtsp://root:001o000000vmbHtAAI@10.86.12.100:554/axis-media/media.amp?videocodec=h264&resolution==768x576&fps=25"}
     },
