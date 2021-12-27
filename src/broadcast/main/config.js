32980035bfb64c58604b7ad7bc28d6628659af38
Arvind Umrao <akumrao@yahoo.com>
2021-12-27 19:13:49 +0530

    added more camera

--- src/broadcast/main/config.js
+++ src/broadcast/main/config.js
@@ -10,9 +10,9 @@
         "729795F1234CAM14528": {"rtsp":"rtsp://root:60056005@10.76.0.16:555/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
         "229795F1234CAM14567": {"rtsp":"rtsp://root:60056006@10.86.8.16:559/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
         "929795F1234CAM14569": {"rtsp":"rtsp://root:60056006@10.86.8.16:560/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
-        "629795F1234CAM14569": {"rtsp":"rtsp://root:001o000000vmbHtAAI@10.86.12.100:554/axis-media/media.amp?videocodec=h264&resolution==768x576&fps=25"},
-        "429795F1234CAM14561": {"rtsp":"rtsp://root:0011J00001MUvfQQAT@10.86.0.24:554/axis-media/media.am?videocodec=h264&resolution==768x576&fps=25"},
-        "529795F1234CAM14562": {"rtsp":"rtsp://root:001o000000vmbHtAAI@10.86.12.100:558/axis-media/media.amp?videocodec=h264&resolution==768x576&fps=25"}
+        "629795F1234CAM14569": {"rtsp":"rtsp://root:001o000000vmbHtAAI@10.86.12.100:554/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
+        "429795F1234CAM14561": {"rtsp":"rtsp://root:0011J00001MUvfQQAT@10.86.0.24:554/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"},
+        "529795F1234CAM14562": {"rtsp":"rtsp://root:001o000000vmbHtAAI@10.86.12.100:558/axis-media/media.amp?videocodec=h264&resolution=768x576&fps=25"}
 
     },
     "logLevel": "info",
