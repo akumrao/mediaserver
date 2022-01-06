ffcb9a77e22e9d2cffdf92eadea3abafd5f3f140
Arvind Umrao <akumrao@yahoo.com>
2022-01-06 13:17:20 +0530

    changed the logfile name to mediaserver

--- src/rtsp/src/Settings.cpp
+++ src/rtsp/src/Settings.cpp
@@ -65,10 +65,10 @@ void Settings::SetConfiguration(json &cnfg)
             base::Level ld = base::getLevelFromString(loglevel.c_str());
             
 #if	LOGGING_LOG_TO_FILE
-            base::Logger::instance().add(new base::FileChannel("mpegdash","/var/log/mpegdash", ld));
+            base::Logger::instance().add(new base::FileChannel("mediaserver","/var/log/mediaserver", ld));
             base::Logger::instance().setWriter(new base::AsyncLogWriter);
 #else
-            base::Logger::instance().add(new base::ConsoleChannel("mpegdash", ld));
+            base::Logger::instance().add(new base::ConsoleChannel("mediaserver", ld));
 #endif
             
         }
