a2b0e3d34f5782be0fa227245f9c474fd8f72cbd
Arvind Umrao <akumrao@yahoo.com>
2022-06-26 00:30:45 +0530

    fixed compilation issues of windows

--- src/ffmpeg/ffmpeg_cpp/mpegaudiodata.h
+++ src/ffmpeg/ffmpeg_cpp/mpegaudiodata.h
@@ -35,7 +35,7 @@
 #define MODE_EXT_I_STEREO  1
 
 extern av_export const uint16_t avpriv_mpa_bitrate_tab[2][3][15];
-extern av_export const uint16_t avpriv_mpa_freq_tab[3];
+extern  const uint16_t avpriv_mpa_freq_tab[3];
 extern const int ff_mpa_sblimit_table[5];
 extern const int ff_mpa_quant_steps[17];
 extern const int ff_mpa_quant_bits[17];
